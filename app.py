31b1eb9f0d8f89b311f36765573edbb42b2afe4f
EGkon <132073553+EGkon@users.noreply.github.com>
2024-10-19 21:53:31 +0300

    hello

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    name = request.form['name']
+    return render_template('greet.html', name=name)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
